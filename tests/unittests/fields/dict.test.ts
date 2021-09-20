2e5729a02931a88ba349646ed64b4dfe366c76da
Ing Petr Jindra <el.mordo@gmail.com>
2021-09-20 18:21:06 +0200

    tests for dict and map (WIP)

---
+++ tests/unittests/fields/dict.test.ts
@@ -0,0 +1,17 @@
+import {Dict, Int, Str} from "../../../lib";
+
+describe("the dict field", () => {
+    let field: Dict;
+
+    beforeEach(() => {
+        field = new Dict(null, new Str(null), new Int(null));
+    })
+
+    it("should load data", () => {
+
+    });
+
+    it("should dump data", () => {
+
+    });
+});
