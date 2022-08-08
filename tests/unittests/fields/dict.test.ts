fe1696967851b324f9af9a2fe533b6a9fda4a23c
Petr Jindra <petr@jindrovi.uno>
2022-08-08 12:00:15 +0200

    update of tests for Dict and Map fields

--- tests/unittests/fields/dict.test.ts
+++ tests/unittests/fields/dict.test.ts
@@ -3,21 +3,28 @@ import {Dict, Int, Str} from "../../../lib";
 describe("the dict field", () => {
     let field: Dict;
 
-    beforeEach(() => {
-        field = new Dict(null, new Str(null), new Int(null));
-    })
+    ([
+        ["when name is null", () => new Dict(null, new Str(null), new Int(null))],
+        ["when name is set", () => new Dict(null, new Str(null), new Int(null))],
+        ["when name is omitted", () => new Dict(null, new Str(null), new Int(null))],
+    ] as [string, () => Dict][]).forEach(([name, factory]) => {
+        describe(name, () => {
+            beforeEach(() => {
+                field = factory();
+            });
+            it("should load data", () => {
+                const data = {foo: 11, bar: 12};
+                const expectedData = {foo: 11, bar: 12};
+                expect(field.load(data)).toEqual(expectedData);
+                expect(field.load(data)).not.toBe(data);
+            });
 
-    it("should load data", () => {
-        const data = {foo: 11, bar: 12};
-        const expectedData = {foo: 11, bar: 12};
-        expect(field.load(data)).toEqual(expectedData);
-        expect(field.load(data)).not.toBe(data);
-    });
-
-    it("should dump data", () => {
-        const data = {foo: 11, bar: 12};
-        const expectedData = {foo: 11, bar: 12};
-        expect(field.dump(data)).toEqual(expectedData);
-        expect(field.dump(data)).not.toBe(data);
+            it("should dump data", () => {
+                const data = {foo: 11, bar: 12};
+                const expectedData = {foo: 11, bar: 12};
+                expect(field.dump(data)).toEqual(expectedData);
+                expect(field.dump(data)).not.toBe(data);
+            });
+        });
     });
 });
--- tests/unittests/fields/map.test.ts
+++ tests/unittests/fields/map.test.ts
@@ -1,23 +1,30 @@
-import {Map_, Int, Str} from "../../../lib";
+import {Int, Map_, Str} from "../../../lib";
 
 describe("the dict field", () => {
     let field: Map_;
 
-    beforeEach(() => {
-        field = new Map_(null, new Str(null), new Int(null));
-    })
+    ([
+        ["when name is null", () => new Map_(null, new Str(null), new Int(null))],
+        ["when name is set", () => new Map_(null, new Str(null), new Int(null))],
+        ["when name is omitted", () => new Map_(null, new Str(null), new Int(null))],
+    ] as [string, () => Map_][]).forEach(([name, factory]) => {
+        describe(name, () => {
+            beforeEach(() => {
+                field = factory();
+            });
+            it("should load data", () => {
+                const data = {foo: 11, bar: 12};
+                const expectedData = new Map([["foo", 11], ["bar", 12]]);
+                expect(field.load(data)).toEqual(expectedData);
+                expect(field.load(data)).not.toBe(data);
+            });
 
-    it("should load data", () => {
-        const data = {foo: 11, bar: 12};
-        const expectedData = new Map([["foo", 11], ["bar", 12]]);
-        expect(field.load(data)).toEqual(expectedData);
-        expect(field.load(data)).not.toBe(data);
-    });
-
-    it("should dump data", () => {
-        const data = new Map([["foo", 11], ["bar", 12]]);
-        const expectedData = {foo: 11, bar: 12};
-        expect(field.dump(data)).toEqual(expectedData);
-        expect(field.dump(data)).not.toBe(data);
+            it("should dump data", () => {
+                const data = new Map([["foo", 11], ["bar", 12]]);
+                const expectedData = {foo: 11, bar: 12};
+                expect(field.dump(data)).toEqual(expectedData);
+                expect(field.dump(data)).not.toBe(data);
+            });
+        });
     });
 });
